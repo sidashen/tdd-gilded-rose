1c5112a9286c77d2e71cf6f84795bc75b78784fc
sidashen <shashasi8023@163.com>
2020-03-23 17:17:56 +0800

    [shasha] refactor: check price is correct

--- src/test/java/cn/xpbootcamp/gilded_rose/ProductTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/ProductTest.java
@@ -3,24 +3,15 @@ package cn.xpbootcamp.gilded_rose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class ProductTest {
 
-  Product product = new Product();
-
   @Test
   void price() {
-    assertEquals(true, isCorrectPrice());
+    Product product = new Product();
+    assumeTrue(product.getQuality() >= 0 && product.getQuality() <= 50);
+//    assertEquals(true, isCorrectPrice());
   }
 
-
-
-  public Boolean isCorrectPrice() {
-    if (product.getQuality() >= 0 && product.getQuality() <= 50) {
-      System.out.println(product.getQuality());
-      return true;
-    } else {
-      return false;
-    }
-  }
 }
