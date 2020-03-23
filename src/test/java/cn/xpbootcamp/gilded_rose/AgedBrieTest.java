2c7a0fcac9a7247d9303db7e3f1860cf0063cb55
sidashen <shashasi8023@163.com>
2020-03-23 23:29:29 +0800

    [shasha] feat: aged brie test

---
+++ src/test/java/cn/xpbootcamp/gilded_rose/AgedBrieTest.java
@@ -0,0 +1,27 @@
+package cn.xpbootcamp.gilded_rose;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class AgedBrieTest {
+
+  @Test
+  @DisplayName("Check price of products")
+  void correct_price() {
+    AgedBrie agedBrie = new AgedBrie(20, 0, "wine");
+    agedBrie.afterFewDays(5);
+    assumeTrue(agedBrie.getQuality() >= 0 && agedBrie.getQuality() <= 50);
+  }
+
+  @Test
+  @DisplayName("Check quality increase with the increase of day")
+  void when_day_add_then_quality_add() {
+    AgedBrie agedBrie = new AgedBrie(20, 0, "wine");
+    agedBrie.afterFewDays(5);
+    assertEquals(25, agedBrie.getQuality());
+  }
+
+}
--- src/test/java/cn/xpbootcamp/gilded_rose/ProductTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/ProductTest.java
@@ -3,6 +3,7 @@ package cn.xpbootcamp.gilded_rose;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
