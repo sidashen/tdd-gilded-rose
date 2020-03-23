e3c1ca3b969c582854a3642e55491b4647638f61
sidashen <shashasi8023@163.com>
2020-03-23 23:34:08 +0800

    [shasha] feat: sulfuras test

---
+++ src/test/java/cn/xpbootcamp/gilded_rose/SulfurasTest.java
@@ -0,0 +1,27 @@
+package cn.xpbootcamp.gilded_rose;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+public class SulfurasTest {
+
+  @Test
+  @DisplayName("Check quality of products")
+  void correct_price() {
+    Sulfuras sulfuras = new Sulfuras(20, 0, "sul");
+    sulfuras.afterFewDays(20);
+    assumeTrue(sulfuras.getQuality() >= 0 && sulfuras.getQuality() <= 50);
+  }
+
+
+  @Test
+  @DisplayName("Check quality do not change")
+  void when_day_add_then_quality_stay() {
+    Sulfuras sulfuras = new Sulfuras(20, 0, "sul");
+    sulfuras.afterFewDays(5);
+    assertEquals(20, sulfuras.getQuality());
+  }
+}
